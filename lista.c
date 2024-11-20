e09bcee94ec876a657b42efc63bc68320e94f7a2
matheusAntunesF <matheusanfr@gmail.com>
2024-11-19 23:25:05 -0300

    Versão final da lista de polinomios

--- lista.c
+++ lista.c
@@ -14,7 +14,7 @@ void define_coeficiente(Polinomio pol, int grau, int coef){
 		return;
 
 	No * no_atual = pol -> prox;
-	while(no_atual -> valor.grau < grau && no_atual -> valor.grau != -1){
+	while(no_atual -> valor.grau < grau && no_atual != pol){
 		no_atual = no_atual -> prox;
 	}
 	
@@ -33,12 +33,21 @@ void define_coeficiente(Polinomio pol, int grau, int coef){
 }
 
 void zera(Polinomio pol){
-	pol = pol -> antec;
-	while(pol -> valor.grau != -1){
-		pol = pol -> antec;
-		free(pol -> prox);
+	No * no_atual = pol -> prox;
+	while(no_atual != pol){
+		no_atual = no_atual -> prox;
+		free(no_atual -> antec);
 	}
 	pol -> prox = pol -> antec = pol;
+}
+
+void insere_novo_res(Polinomio * ap_atual_res, int grau, int coef){
+	No * novo_no = (No *) malloc(sizeof(No));
+	novo_no -> valor.grau = grau;
+	novo_no -> valor.coef = coef;
+	(*ap_atual_res) -> prox = novo_no;
+	novo_no -> antec = (*ap_atual_res);
+	(*ap_atual_res) = (*ap_atual_res) -> prox;
 }
 
 void soma(Polinomio res, Polinomio a, Polinomio b){
@@ -47,61 +56,44 @@ void soma(Polinomio res, Polinomio a, Polinomio b){
 	No * no_atual_a = a -> prox;
 	No * no_atual_b = b -> prox;
 	No * no_atual_res = res;	
-	int novo_coef;
+	int coef, grau;
 	while(no_atual_a != a || no_atual_b != b){
 		if(no_atual_a == a){
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_b -> valor.grau;
-			novo_no -> valor.coef = no_atual_b -> valor.coef; 
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
-			no_atual_res = no_atual_res -> prox;
+			grau = no_atual_b -> valor.grau;
+			coef = no_atual_b -> valor.coef;
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_b = no_atual_b -> prox;
 		} else if(no_atual_b == b){
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_a -> valor.grau;
-			novo_no -> valor.coef = no_atual_a -> valor.coef; 
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
-			no_atual_res = no_atual_res -> prox;
+			grau = no_atual_a -> valor.grau;
+			coef = no_atual_a -> valor.coef;
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_a = no_atual_a -> prox;
 		} else if(no_atual_a -> valor.grau == no_atual_b -> valor.grau){
-			novo_coef = (no_atual_a -> valor.coef) + (no_atual_b -> valor.coef);
-			if(novo_coef == 0){
+			coef = (no_atual_a -> valor.coef) + (no_atual_b -> valor.coef);
+			if(coef == 0){
 				no_atual_a = no_atual_a -> prox;
 				no_atual_b = no_atual_b -> prox;
 			}else {
-				No * novo_no = (No *) malloc(sizeof(No));
-				novo_no -> valor.grau = no_atual_a -> valor.grau;
-				novo_no -> valor.coef = novo_coef; 
-				no_atual_res -> prox = novo_no;
-				novo_no -> antec = no_atual_res;
+				grau = no_atual_a -> valor.grau;
+				insere_novo_res(&no_atual_res, grau, coef);
 				no_atual_a = no_atual_a -> prox;
 				no_atual_b = no_atual_b -> prox;
-				no_atual_res = no_atual_res -> prox;
 			}
 		} else if(no_atual_a -> valor.grau > no_atual_b -> valor.grau){
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_b -> valor.grau;
-			novo_no -> valor.coef = no_atual_b -> valor.coef;
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
+			grau = no_atual_b -> valor.grau;
+			coef = no_atual_b -> valor.coef;
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_b = no_atual_b -> prox;
-			no_atual_res = no_atual_res -> prox;
 		} else{ 
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_a -> valor.grau;
-			novo_no -> valor.coef = no_atual_a -> valor.coef;
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
+			grau = no_atual_a -> valor.grau;
+			coef = no_atual_a -> valor.coef;
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_a = no_atual_a -> prox;
-			no_atual_res = no_atual_res -> prox;
 		}
 	}
 	no_atual_res -> prox = res;
 	res -> antec = no_atual_res;
 }
-
 
 void subtrai(Polinomio res, Polinomio a, Polinomio b){
 	zera(res);
@@ -109,55 +101,39 @@ void subtrai(Polinomio res, Polinomio a, Polinomio b){
 	No * no_atual_a = a -> prox;
 	No * no_atual_b = b -> prox;
 	No * no_atual_res = res;	
-	int novo_coef;
+	int coef, grau;
 	while(no_atual_a != a || no_atual_b != b){
 		if(no_atual_a == a){
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_b -> valor.grau;
-			novo_no -> valor.coef = -(no_atual_b -> valor.coef); 
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
-			no_atual_res = no_atual_res -> prox;
+			grau = no_atual_b -> valor.grau;
+			coef = -(no_atual_b -> valor.coef);
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_b = no_atual_b -> prox;
 		} else if(no_atual_b == b){
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_a -> valor.grau;
-			novo_no -> valor.coef = no_atual_a -> valor.coef; 
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
-			no_atual_res = no_atual_res -> prox;
+			grau = no_atual_a -> valor.grau;
+			coef = no_atual_a -> valor.coef;
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_a = no_atual_a -> prox;
 		} else if(no_atual_a -> valor.grau == no_atual_b -> valor.grau){
-			novo_coef = (no_atual_a -> valor.coef) - (no_atual_b -> valor.coef);
-			if(novo_coef == 0){
+			coef = (no_atual_a -> valor.coef) - (no_atual_b -> valor.coef);
+			if(coef == 0){
 				no_atual_a = no_atual_a -> prox;
 				no_atual_b = no_atual_b -> prox;
 			} else {
-				No * novo_no = (No *) malloc(sizeof(No));
-				novo_no -> valor.grau = no_atual_a -> valor.grau;
-				novo_no -> valor.coef = novo_coef; 
-				no_atual_res -> prox = novo_no;
-				novo_no -> antec = no_atual_res;
+				grau = no_atual_a -> valor.grau;
+				insere_novo_res(&no_atual_res, grau, coef);
 				no_atual_a = no_atual_a -> prox;
 				no_atual_b = no_atual_b -> prox;
-				no_atual_res = no_atual_res -> prox;
 			}
 		} else if(no_atual_a -> valor.grau > no_atual_b -> valor.grau){
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_b -> valor.grau;
-			novo_no -> valor.coef = -(no_atual_b -> valor.coef);
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
+			grau = no_atual_b -> valor.grau;
+			coef = -(no_atual_b -> valor.coef);
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_b = no_atual_b -> prox;
-			no_atual_res = no_atual_res -> prox;
 		} else{ 
-			No * novo_no = (No *) malloc(sizeof(No));
-			novo_no -> valor.grau = no_atual_a -> valor.grau;
-			novo_no -> valor.coef = no_atual_a -> valor.coef;
-			no_atual_res -> prox = novo_no;
-			novo_no -> antec = no_atual_res;
+			grau = no_atual_a -> valor.grau;
+			coef = no_atual_a -> valor.coef;
+			insere_novo_res(&no_atual_res, grau, coef);
 			no_atual_a = no_atual_a -> prox;
-			no_atual_res = no_atual_res -> prox;
 		}
 	}
 	no_atual_res -> prox = res;
@@ -165,21 +141,21 @@ void subtrai(Polinomio res, Polinomio a, Polinomio b){
 }
 
 void imprime(Polinomio pol){
-	pol = pol -> prox;
+	No * no_atual = pol -> prox;
 	printf("[");
-	while(pol -> prox -> valor.grau != -1){
-		printf("(%d,%d),", pol -> valor.grau, pol -> valor.coef);
-		pol = pol -> prox;
+	while(no_atual -> prox != pol){
+		printf("(%d,%d),", no_atual -> valor.grau, no_atual -> valor.coef);
+		no_atual = no_atual -> prox;
 	}
-	printf("(%d,%d)]\n", pol -> valor.grau, pol -> valor.coef);
+	printf("(%d,%d)]\n", no_atual -> valor.grau, no_atual -> valor.coef);
 }
 
 void desaloca_polinomio(Polinomio *ap_pol){
 	No * no_atual = (*ap_pol);
-	no_atual = no_atual -> antec;
-	while(no_atual -> valor.grau != -1){
-		no_atual = no_atual -> antec;
-		free(no_atual -> prox);
+	no_atual = no_atual -> prox;
+	while(no_atual != (*ap_pol)){
+		no_atual = no_atual -> prox;
+		free(no_atual -> antec);
 	}
 	free(no_atual);
 	(*ap_pol)=NULL;
